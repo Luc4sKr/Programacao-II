567dfa9a1ab920254cf7d008ca2e5e57b72fee32
Lucas <lucaskreuch77@gmail.com>
2023-02-23 16:54:40 -0300

    jquery

--- js/001-jquery/scripts/script.js
+++ js/001-jquery/scripts/script.js
@@ -1,4 +1,22 @@
-$("#btn").clikc(function() {
-    $("#img").toggleClass("hide");
-    $("#img").toggleClass("");
+$(document).ready(() => {
+
+    $(window).mousemove((event) => {
+        $("#marreta").css({
+            top: event.pageY - $("#marreta").outerHeight(true) / 2,
+            left: event.pageX - $("#marreta").outerWidth(true) / 2
+        });
+    });
+
+    $("#cachorro").click(() => {
+        $("#cachorro").css({
+            top: getRandom(0, $(".game").outerHeight(true) - $("#cachorro").outerHeight(true)),
+            left: getRandom(0, $(".game").outerWidth(true) - $("#cachorro").outerWidth(true))
+        });
+    });
+
 });
+
+
+function getRandom(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
