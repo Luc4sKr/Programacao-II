d385d34169abb2196fb980554e8eae78f5bd8e9b
Lucas <lucaskreuch77@gmail.com>
2023-03-25 12:38:20 -0300

    frontend

---
+++ python/008-python_with_vue/frontend/src/App.vue
@@ -0,0 +1,41 @@
+<template>
+    <HeaderComponent />
+    <FormPessoa />
+</template>
+
+
+<script>
+import HeaderComponent from './components/HeaderComponent.vue';
+import FormPessoa from './components/FormPessoa.vue';
+
+export default {
+    name: 'App',
+    components: {
+        HeaderComponent,
+        FormPessoa
+    }
+}
+</script>
+
+
+<style>
+:root {
+    --green: #41B883;
+    --bluish-gray: #34495E;
+}
+
+* {
+    margin: 0px;
+    padding: 0px;
+}
+
+#app {
+    font-family: Avenir, Helvetica, Arial, sans-serif;
+    color: var(--bluish-gray);
+}
+
+.form-container {
+    display: block;
+    
+}
+</style>
---
+++ python/008-python_with_vue/frontend/src/components/FormPessoa.vue
@@ -0,0 +1,26 @@
+<template>
+    <div class="form-container">
+        <form id="form-pessoa" action="post">
+        <div class="form-controller">
+            <label for="nome">Nome</label>
+            <input type="text" name="nome" id="nome">
+        </div>
+        <div class="form-controller">
+            <label for="email">Email</label>
+            <input type="text" name="nome" id="nome">
+        </div>
+    </form>
+    </div>
+</template>
+
+
+<script>
+export default {
+    name: "FormPessoa"
+}
+</script>
+
+
+<style scoped>
+
+</style>
