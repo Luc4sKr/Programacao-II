c450cb65e12e53937ed87948069d7886e876c846
Lucas <lucaskreuch77@gmail.com>
2023-03-24 21:27:51 -0300

    vue

---
+++ vue/002-currency-converter/src/App.vue
@@ -0,0 +1,30 @@
+<template>
+	<h1>Conversor</h1>
+	<div id="app">
+		<ConversorComponent coinA="USD" coinB="BRL"/>
+	</div>
+</template>
+
+
+<script>
+import ConversorComponent from "./components/ConversorComponent.vue";
+
+export default {
+	name: 'App',
+	components: {
+		ConversorComponent
+	}
+}
+</script>
+
+
+<style>
+#app {
+	font-family: Avenir, Helvetica, Arial, sans-serif;
+	-webkit-font-smoothing: antialiased;
+	-moz-osx-font-smoothing: grayscale;
+	text-align: center;
+	color: #2c3e50;
+	margin-top: 60px;
+}
+</style>
---
+++ vue/002-currency-converter/src/components/ConversorComponent.vue
@@ -0,0 +1,34 @@
+<template>
+    <div class="conversor">
+        <h2>{{ coinA }} para {{ coinB }}</h2>
+        <input type="text" v-moel="coinA_value" v-bind:placeholder="coinA">
+		<input type="button" value="convert">
+        <h3>{{ coinB_value }}</h3>
+    </div>
+</template>
+
+
+<script>
+export default {
+    name: "ConversorComponent",
+    props: ["coinA", "coinB"],
+
+    data() {
+        return {
+            coinA_value: "",
+            coinB_value: 0
+        }
+    },
+
+    methods: {
+        convert() {
+            
+        }
+    }
+}
+</script>
+
+
+<style scoped>
+
+</style>
