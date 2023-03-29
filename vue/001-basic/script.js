c78f229986a2ba72a5cb8ae4e347054605b4684c
Lucas <lucaskreuch77@gmail.com>
2023-03-29 16:56:14 -0300

    vue | basic

--- vue/001-basic/script.js
+++ vue/001-basic/script.js
@@ -1,66 +1,12 @@
 new Vue({
-    el: '#app',
-    template: `
-      <div class="container">
-        <h1>Comentários</h1>
-        <hr />
-        <div class="form-todo form-group">
-          <p>
-            <input placeholder="nome" type="text" name="author" class="form-control" v-model="name" />
-          </p>
-          <p>
-            <textarea placeholder="Comentário" name="message"  class="form-control" v-model="message"></textarea>
-          </p>
-          <button v-on:click="addComment" type="submit" class="btn btn-primary">Comentar</button>
-        </div>
-        <div class="list-group mt-3">
-          <div class="list-group-item" v-for="(comment, index) in allComments">
-            <span class="comment__author">Autor: <strong>{{ comment.name }}</strong></span>
-            <p>{{ comment.message }}</p>
-            <div>
-              <a href="#" title="Excluir" v-on:click.prevent="removeComment(index)">Excluir</a>
-            </div>
-          </div>
-        </div>
-        <hr />
-      </div>
-    `,
-    data() {
-        return {
-            comments: [],
-            name: '',
-            message: ''
-        }
-    },
-    methods: {
-        addComment() {
-            if (this.message.trim() === '') {
-                return;
-            }
-
-            this.comments.push({
-                name: this.name,
-                message: this.message
-            });
-
-            this.name = '';
-            this.message = '';
-        },
-        removeComment(index) {
-            this.comments.splice(index, 1);
-        }
-    },
-    computed: {
-        allComments() {
-            return this.comments.map(comment => ({
-                ...comment,
-                name: comment.name.trim() === '' ? 'Anônimo' : comment.name
-            }))
-        }
-    },
-    watch: {
-        comments(val) {
-            console.log('val', val)
-        }
-    }
-})
+  el: '#app',
+  data: {
+      mensagem: 'Olá VueJS',
+      nome: ''
+  },
+  methods: {
+      mudarNome: function(event) {
+          this.nome = event.target.value;
+      }
+  }
+});
