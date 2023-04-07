af15652e82b415d8953cf24e3ef312abd7207541
Lucas <lucaskreuch77@gmail.com>
2023-04-06 22:20:27 -0300

    vue | lista de filmes

---
+++ vue/004-lista-de-filmes/src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+
+    <div class="p-5 mb-4 bg-primary text-white rounded">
+      <div class="container">
+        <h1 class="display-4">Lista de Filmes</h1>
+        <p class="lead">Treinando comunicação entre Components no Vue</p>
+      </div>
+    </div>
+
+    <div class="container">
+      <FilmesLista/>
+    </div>
+
+  </div>
+</template>
+
+<script>
+import FilmesLista from './components/FilmesLista.vue'
+
+export default {
+  components: {
+    FilmesLista
+  }
+}
+</script>
---
+++ vue/004-lista-de-filmes/src/components/FilmesListaIten.vue
@@ -0,0 +1,6 @@
+<template>
+    <li class="list-group-item d-flex justify-content-between">
+        <span>Vingadores: Guerra Infinita</span>
+        <button class="btn btn-success">Editar</button>
+    </li>
+</template>
