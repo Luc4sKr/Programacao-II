479bd40b9b455d5811be3df08c4c04c4304b6cbe
Lucas <lucaskreuch77@gmail.com>
2023-04-06 23:13:06 -0300

    vue | lista de filmes | validando props

--- vue/004-lista-de-filmes/src/components/FilmesListaIten.vue
+++ vue/004-lista-de-filmes/src/components/FilmesListaIten.vue
@@ -1,6 +1,6 @@
 <template>
     <li class="list-group-item d-flex justify-content-between">
-        <span>{{ tituloFilme }}</span>
+        <span>{{ tituloFilmeConcatenado }}</span>
         <button class="btn btn-success">Selecionar</button>
     </li>
 </template>
@@ -8,6 +8,22 @@
 
 <script>
 export default {
-    props: ["tituloFilme"]
+    props: {
+        tituloFilme: {
+            type: String,
+            required: true,
+            validator() {
+                return true;
+            }
+        }
+    },
+    created() {
+        console.log("Título do filme: " + this.tituloFilme);
+    },
+    computed: {
+        tituloFilmeConcatenado() {
+            return `Título: ${this.tituloFilme}`
+        }
+    }
 }
 </script>
