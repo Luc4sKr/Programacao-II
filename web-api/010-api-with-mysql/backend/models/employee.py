e4412017eda384a1f75085009a8bfec56b4bbe17
Luc4sKr <lucaskreuch77@gmail.com>
2023-05-18 11:36:27 -0300

    update

--- web-api/010-api-with-mysql/backend/models/employee.py
+++ web-api/010-api-with-mysql/backend/models/employee.py
@@ -1,6 +1,5 @@
 from public.configs import *
 from .person import Person
-from .departament import Departament
 
 class Employee(db.Model):
     id = db.Column(db.Integer, primary_key=True, nullable=False)
@@ -10,9 +9,9 @@ class Employee(db.Model):
     person_id = db.Column(db.Integer, db.ForeignKey(Person.id), nullable=False)
     person = db.relationship("Person")
 
-    departament_id = db.Column(db.Integer, db.ForeignKey(Departament.id), nullable=False)
-    departament = db.relationship("Departament")
+    
 
+    
     def __str__(self):
         return f"{self.id}, {self.salary}, {self.position}, {self.person}"
     
--- web-api/010-api-with-mysql/backend/public/configs.py
+++ web-api/010-api-with-mysql/backend/public/configs.py
@@ -3,7 +3,7 @@ from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 
-app.config['SQLALCHEMY_DATABASE_URI'] = "mysql+pymysql://root:admin@localhost/teste"
+app.config['SQLALCHEMY_DATABASE_URI'] = "mysql+pymysql://root:root@localhost/teste"
 
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
