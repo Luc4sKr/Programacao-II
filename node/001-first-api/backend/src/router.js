b15506ad3005c984bb92df22486a30fbfc46f68c
Lucas <lucaskreuch77@gmail.com>
2023-05-18 21:19:19 -0300

    node | middlewares

--- node/001-first-api/backend/src/router.js
+++ node/001-first-api/backend/src/router.js
@@ -1,9 +1,11 @@
 const express = require("express");
-const tasksController = require("./controllers/tasksController");
 
 const router = express.Router();
 
+const tasksController = require("./controllers/tasksController");
+const taskMiddleware = require("./middlewares/tasksMiddleware");
+
 router.get("/tasks", tasksController.getAll);
-router.post("/tasks", tasksController.createTask);
+router.post("/tasks", taskMiddleware.validateBody, tasksController.createTask);
 
 module.exports = router;
